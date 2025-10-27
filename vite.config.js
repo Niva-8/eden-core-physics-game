6327d5c7037b1fa53f4897130567251e6347fe9d
Niva_8 <niva_8_proof@proton.me>
2025-10-27 19:34:26 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: ".",
-  publicDir: "public",
-  build: {
-    outDir: "dist",
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
 });
