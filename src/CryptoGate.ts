bbe1230aa7119f28846a587e01789a50f921745c
Niva_8 <niva_8_proof@proton.me>
2025-10-27 19:50:12 -0400

    Create CryptoGate.ts

---
+++ src/CryptoGate.ts
@@ -0,0 +1,9 @@
+export function initCryptoGates() {
+  console.log('Crypto payment gates initialized')
+
+  const btc = 'bc1qgv3zcs6jgudzueuxtdqeawjstgpujtykl92xge'
+  const eth = '0xE66DA2e994E2e1F267f5d9c3A80Be847863fcA63'
+
+  console.log(`Bitcoin: ${btc}`)
+  console.log(`Ethereum: ${eth}`)
+}
